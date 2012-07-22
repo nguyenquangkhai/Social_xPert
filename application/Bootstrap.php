0cdfda49927bcaebaf0be60f3c048ee7c97a911e
Nguyen Quang Khai <redfox273@gmail.com>
2012-07-22 10:51:32 +0700

    Multi DB implement

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,5 +7,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view = $this->getResource('view');
         $view->doctype('HTML5');
     }
+    
+    protected function _initDbRegistry(){
+        $this->bootstrap('multidb');
+        $multidb = $this->getPluginResource('multidb');
+        Zend_Registry::set('default', $multidb->getDb('web'));
+        Zend_Registry::set('facebook', $multidb->getDb('facebook'));
+        Zend_Registry::set('log', $multidb->getDb('log'));
+    }
 }
 

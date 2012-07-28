26f05d6fe96b5011ec593ad3567073e69055417f
nguyenquangkhai <redfoxs273@gmail.com>
2012-07-28 13:57:27 +0700

    post function

--- application/models/DbMapper/PostMapper.php
+++ application/models/DbMapper/PostMapper.php
@@ -22,6 +22,7 @@ class Application_Model_DbMapper_PostMapper {
         return $this->_dbTable;
     }
 
+    //FIND OBJECT SECTION
     public function findById($post_id) {
         $result = $this->getDbTable()->find($post_id);
         if (0 == count($result)) {
@@ -41,27 +42,36 @@ class Application_Model_DbMapper_PostMapper {
     
     public function findByPage($page_id){
         $table = $this->getDbTable();
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id);
+        $resultSet = $table->fetchAll($select);
         
+        return $resultSet;
     }
     
-    public function findLastest($num = 1){
+    public function findLastest($page_id, $num = 1){
         $table = $this->getDbTable();
         $select = $table->select();
-        $select->order("addtime DESC")
+        $select->where("page_id = ?",$page_id)
+               ->order("addtime DESC")
                ->limit($num);
-        
-        $resultSet = $table->current();
+        if($num == 1)
+            $resultSet = $table->fetchRow($select);
+        else
+            $resultSet = $table->fetchAll($select);
         
         return $resultSet;
     }
 
-    public function findByAddtime($fromDate, $toDate = False) {
+    public function findByAddtime($page_id, $fromDate, $toDate = False) {
         $table = $this->getDbTable();
         $select = $table->select();
         if(!$toDate){
-            $select->where("addtime >= '?'", $fromDate);
+            $select->where("page_id = ?", $page_id)
+                   ->where("addtime >= '?'", $fromDate);
         }else{
-            $select->where("addtime >= '?'",$fromDate)
+            $select->where("page_id = ?",$page_id)
+                   ->where("addtime >= '?'",$fromDate)
                    ->where("addtime <= '?'",$toDate);
         }
         
@@ -70,5 +80,100 @@ class Application_Model_DbMapper_PostMapper {
         return $resultSet;
     }
     
+    public function findByPosted($page_id, $is_posted = 1){
+        //$posted : 1 = has already posted | 0 = is not posted yet
+        $table = $this->getDbTable();
+        $select = $table->select();
+        $select->where("is_posted = ?", $is_posted);
+        $resultSet = $table->fetchAll($select);
+        
+        return $resultSet;
+    }
+    
+    //UPDATE OBJECT SECTION
+    public function updateImage($page_id, $post_id, $image_id){
+        $table = $this->getDbTable();
+        
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id)
+               ->where("post_id = ?",$post_id);
+        
+        $row = $table->fetchRow($select);
+        if($row != null){
+            $row->image_id = $image_id;
+            $row->save();
+            return $row;
+        }else{
+            return null;
+        }
+    }
+    
+    public function updateVideo($page_id, $post_id, $video_id){
+        $table = $this->getDbTable();
+        
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id)
+               ->where("post_id = ?",$post_id);
+        
+        $row = $table->fetchRow($select);
+        if($row != null){
+            $row->video_id = $video_id;
+            $row->save();
+            return $row;
+        }else{
+            return null;
+        }
+    }
+    
+    public function updatePoll($page_id, $post_id, $poll_id){
+        $table = $this->getDbTable();
+        
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id)
+               ->where("post_id = ?",$post_id);
+        
+        $row = $table->fetchRow($select);
+        if($row != null){
+            $row->poll_id = $poll_id;
+            $row->save();
+            return $row;
+        }else{
+            return null;
+        }
+    }
+    
+    public function updateMilestone($page_id, $post_id, $milestone_id){
+        $table = $this->getDbTable();
+        
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id)
+               ->where("post_id = ?",$post_id);
+        
+        $row = $table->fetchRow($select);
+        if($row != null){
+            $row->milestone_id = $milestone_id;
+            $row->save();
+            return $row;
+        }else{
+            return null;
+        }
+    }
+    
+    public function updateEvent($page_id, $post_id, $event_id){
+        $table = $this->getDbTable();
+        
+        $select = $table->select();
+        $select->where("page_id = ?",$page_id)
+               ->where("post_id = ?",$post_id);
+        
+        $row = $table->fetchRow($select);
+        if($row != null){
+            $row->event_id = $event_id;
+            $row->save();
+            return $row;
+        }else{
+            return null;
+        }
+    }
 }
 
